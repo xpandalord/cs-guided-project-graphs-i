52690a0648eb462289b514a186595e16b4c6b637
Andy Tran <xpandalord@gmail.com>
2020-10-14 19:53:46 -0400

    updates

--- src/demonstration_1.py
+++ src/demonstration_1.py
@@ -1,24 +1,74 @@
 """
 You are given an undirected graph with its maximum degree (the degree of a node
 is the number of edges connected to the node).
-
+​
 You need to write a function that can take an undirected graph as its argument
 and color the graph legally (a legal graph coloring is when no adjacent nodes
 have the same color).
-
+​
 The number of colors necessary to complete a legal coloring is always one more
 than the graph's maximum degree.
-
+​
 *Note: We can color a graph in linear time and space. Also, make sure that your
 solution can handle a loop in a reasonable way.*
 """
+colors = set(["Red", "Green", "Blue", "Purple", "Yellow"])
+​
 # Definition for a graph node.
 class GraphNode:
     def __init__(self, label):
         self.label = label
         self.neighbors = set()
         self.color = None
-
+​
+# brute force approach 
+# `graph` is a list of GraphNodes 
+# V: number of nodes/vertices in the graph 
+# E: number of edges/connections
+# C: number of possible colors 
+​
+# O(V * E * C)
 def color_graph(graph, colors):
     # Your code here
-
+    # iterate through the nodes in our graph 
+    for node in graph: # O(V)
+        # figure out what colors are illegal based on the neighbors' colors 
+        # illegal_colors = set([neighbor.color for neighbor in node.neighbors if neighbor.color])
+        illegal_colors = {neighbor.color for neighbor in node.neighbors if neighbor.color}
+​
+        # for neighbor in node.neighbors: # O(E)
+        #     if neighbor.color is not None:
+        #         illegal_colors.add(neighbor.color)
+​
+        # pick a color that isn't taken by a neighbor 
+        for color in colors: # O(C)
+            if color not in illegal_colors: # O(1)
+                node.color = color
+                break
+​
+# Construct a undirected graph 
+g1 = GraphNode(1)
+g2 = GraphNode(2)
+g3 = GraphNode(3)
+g4 = GraphNode(4)
+g5 = GraphNode(5)
+​
+nodes = [g1, g2, g3, g4, g5]
+​
+g1.neighbors.add(g2)
+g1.neighbors.add(g3)
+g1.neighbors.add(g4)
+g2.neighbors.add(g1)
+g2.neighbors.add(g4)
+g2.neighbors.add(g5)
+g2.neighbors.add(g3)
+g3.neighbors.add(g1)
+g3.neighbors.add(g2)
+g4.neighbors.add(g1)
+g4.neighbors.add(g2)
+g5.neighbors.add(g2)
+​
+color_graph(nodes, colors)
+​
+for node in nodes:
+    print(node.label, node.color)
--- src/demonstration_2.py
+++ src/demonstration_2.py
@@ -1,12 +1,12 @@
 """
 You are given a 2d grid of `"1"`s and `"0"`s that represents a "map". The
 `"1"`s represent land and the `"0"s` represent water.
-
+​
 You need to write a function that, given a "map" as an argument, counts the
 number of islands. Islands are defined as adjacent pieces of land that are
 connected horizontally or vertically. You can also assume that the edges of the
 map are surrounded by water.
-
+​
 Example 1:
 Input: grid = [
   ["1","1","1","1","0"],
@@ -15,7 +15,7 @@ Input: grid = [
   ["0","0","0","0","0"]
 ]
 Output: 1
-
+​
 Example 2:
 Input: grid = [
   ["1","1","0","0","0"],
@@ -26,7 +26,65 @@ Input: grid = [
 Output: 3
 """
 from collections import deque
-
+​
 def numIslands(grid):
     # Your code here
-
+    counter = 0
+    # we need to find 1's 
+    # iterate through our matrix using a nested loop 
+    for row_index, row in enumerate(grid):
+        for col_index, digit in enumerate(row):
+            # what happens when we see a 1? 
+            if digit == "1":
+                # increment our counter 
+                counter += 1
+                # we also need to make sure we don't double count 
+                # mutate our input grid
+                # if we see a 1, after we've processed it, let's just set it 0 
+                grid[row_index][col_index] = 0
+                # figure out how far this island extends in the 
+                # horizontal and vertical directions 
+                queue = deque([(row_index, col_index)])
+​
+                # we need to process the 1's adjacent to the current 1 
+                while len(queue) > 0:
+                    r, c = queue.popleft()
+                    # check the 4 cardinal directions around the current 1 
+                    # if we see any other 1s, add it to queue
+​
+                    # look north 
+                    if r > 0 and grid[r-1][c] == '1':
+                        grid[r-1][c] = 0
+                        queue.append((r-1, c))
+​
+                    # look south
+                    if r < len(grid) - 1 and grid[r+1][c] == '1':
+                        grid[r+1][c] = 0
+                        queue.append((r+1, c))
+​
+                    # look east 
+                    if c < len(row) - 1 and grid[r][c+1] == '1':
+                        grid[r][c+1] = 0
+                        queue.append((r, c+1))
+​
+                    # look west 
+                    if c > 0 and grid[r][c-1] == '1':
+                        grid[r][c-1] = 0
+                        queue.append((r, c-1))
+​
+    return counter 
+                
+grid1 = [
+  ["1","1","1","1","0"],
+  ["1","1","0","1","0"],
+  ["1","1","0","0","0"],
+  ["0","0","0","0","0"]
+]
+grid2 = [
+  ["1","1","0","0","0"],
+  ["1","1","0","0","0"],
+  ["0","0","1","0","0"],
+  ["0","0","0","1","1"]
+]
+​
+print(numIslands(grid1))
